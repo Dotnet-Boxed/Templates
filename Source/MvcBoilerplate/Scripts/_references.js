2554c0cb0b1b2c45dc5d0de07d0a2cc9d8518b7b
RehanSaeed <rehansaeed@gmail.com>
2014-11-09 15:00:08 +0000

    New Project

---
+++ Source/MvcBoilerplate/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-1.9.0.js" />
+/// <reference path="jquery.validate.unobtrusive.min.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="respond.min.js" />
+/// <reference path="respond.matchmedia.addlistener.min.js" />
