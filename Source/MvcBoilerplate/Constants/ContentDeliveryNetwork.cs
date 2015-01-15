b4243fa8b6bb6222fc168e3f138cecf81f8c98b9
RehanSaeed <rehansaeed@gmail.com>
2015-01-15 07:14:12 +0000

    1.0.8

--- Source/MvcBoilerplate/Constants/ContentDeliveryNetwork.cs
+++ Source/MvcBoilerplate/Constants/ContentDeliveryNetwork.cs
@@ -5,7 +5,7 @@
         public static class Google
         {
             public const string Domain = "ajax.googleapis.com";
-            public const string JQueryUrl = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
+            public const string JQueryUrl = "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
         }
 
         public static class Microsoft
--- Source/MvcBoilerplateTemplate/Scripts/_references.js
+++ Source/MvcBoilerplateTemplate/Scripts/_references.js
@@ -1,5 +1,5 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="jquery-2.1.1.js" />
+/// <reference path="jquery-2.1.3.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="respond.js" />
