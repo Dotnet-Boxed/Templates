1bd3c865640f96b392aacf034d7dea52a770b109
RehanSaeed <rehansaeed@gmail.com>
2015-03-21 12:25:43 +0000

    Version 1.0.20 - Updated ReadMe.html with information about using the https://www.ssllabs.com/ssltest site to check that you have imlemented SSL/TLS over HTTPS correctly.
    Version 1.0.19
    - Microsoft CDN jQuery Validation URL updated to 1.13.1.
    - Microsoft CDN jQuery Validation Unobtrusive URL updated to 5.2.3.
    - Microsoft CDN Modernizr URL updated to 2.8.3.
    - Microsoft CDN Bootstrap URL updated to 3.3.2.
    - Microsoft CDN Respond URL updated to 1.4.2.

--- Source/MvcBoilerplate/Constants/ContentDeliveryNetwork.cs
+++ Source/MvcBoilerplate/Constants/ContentDeliveryNetwork.cs
@@ -11,11 +11,11 @@
         public static class Microsoft
         {
             public const string Domain = "ajax.aspnetcdn.com";
-            public const string JQueryValidateUrl = "//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js";
-            public const string JQueryValidateUnobtrusiveUrl = "//ajax.aspnetcdn.com/ajax/mvc/5.1/jquery.validate.unobtrusive.min.js";
-            public const string ModernizrUrl = "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.7.2.js";
-            public const string BootstrapUrl = "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.1/bootstrap.min.js";
-            public const string RespondUrl = "//ajax.aspnetcdn.com/ajax/respond/1.2.0/respond.js";
+            public const string JQueryValidateUrl = "//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js";
+            public const string JQueryValidateUnobtrusiveUrl = "//ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js";
+            public const string ModernizrUrl = "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.8.3.js";
+            public const string BootstrapUrl = "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.2/bootstrap.min.js";
+            public const string RespondUrl = "//ajax.aspnetcdn.com/ajax/respond/1.4.2/respond.js";
         }
     }
 }
