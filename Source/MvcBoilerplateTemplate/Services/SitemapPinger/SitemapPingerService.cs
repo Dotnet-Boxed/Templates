a96e77c91dc4d5b8d4f160374abf59f8c80734bc
RehanSaeed <rehansaeed@gmail.com>
2015-04-28 18:56:24 +0100

    Version 1.0.36 - Fixed the RedirectToHttpsAttribute class not being recognized in the project, thanks to @dls314159 for highlighting this issue. - Added two more IgnoreRoute statements to support Elmah, copied from Elmah.MVC (This should be part of Elmah.MVC already, have submitted a pull request to Elmah.MVC. If they accept it, we can remove this code altogether). - Added SitemapPingerService.cs and ISitemapPingerService.cs which sends the URL for the sitemap.xml file to Google, Microsoft and Yahoo. - Registered the ISitemapPingerService with Autofac in Startup.Container.cs. - Added a note in ReadMe.html to call the SitemapPingerService's PingSearchEngines method when your sitemap changes.

---
+++ Source/MvcBoilerplateTemplate/Services/SitemapPinger/SitemapPingerService.cs
@@ -0,0 +1,84 @@
+﻿namespace $safeprojectname$.Services
+{
+    using System.Globalization;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using $safeprojectname$.Constants;
+    using $safeprojectname$.Framework;
+
+    public class SitemapPingerService : ISitemapPingerService
+    {
+        #region Fields
+
+        /// <summary>
+        /// The URL's provided by search engines where we can send the location of our sitemap.
+        /// </summary>
+        private static readonly string[] SitemapPingLocations = new string[]
+        {
+            // Google
+            "https://www.google.com/ping?sitemap=",
+            // Bing and Yahoo share the same sitemap ping URL.
+            "http://www.bing.com/ping?sitemap="
+        };
+
+        private readonly HttpClient httpClient;
+        private readonly ILoggingService loggingService;
+        private readonly UrlHelper urlHelper;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SitemapPingerService"/> class.
+        /// </summary>
+        /// <param name="loggingService">The logging service.</param>
+        /// <param name="urlHelper">The URL helper.</param>
+        public SitemapPingerService(
+            ILoggingService loggingService,
+            UrlHelper urlHelper)
+        {
+            this.loggingService = loggingService;
+            this.urlHelper = urlHelper;
+            this.httpClient = new HttpClient();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Send (or 'ping') the URL of this sites sitemap.xml file to search engines like Google, Bing and Yahoo.
+        /// This method should be called each time the sitemap changes. Note that this function will do nothing 
+        /// when the solution configuration is set to Debug mode, so that search engines are not spammed.
+        /// The way we 'ping' our sitemap to search engines is actually an open standard (See http://www.sitemaps.org/protocol.html#submit_ping).
+        /// You can read the sitemap ping documentation for the top search engines below:
+        /// Google - http://googlewebmastercentral.blogspot.co.uk/2014/10/best-practices-for-xml-sitemaps-rssatom.html
+        /// Bing - http://www.bing.com/webmaster/help/how-to-submit-sitemaps-82a15bd4.
+        /// Yahoo - https://developer.yahoo.com/search/siteexplorer/V1/ping.html
+        /// </summary>
+        public async Task PingSearchEngines()
+        {
+#if Release
+            foreach (string sitemapPingLocation in SitemapPingLocations)
+            {
+                string url = sitemapPingLocation + this.urlHelper.Encode(this.urlHelper.AbsoluteRouteUrl(HomeControllerRoute.GetSitemapXml));
+                HttpResponseMessage response = await this.httpClient.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    HttpRequestException exception = new HttpRequestException(string.Format(
+                        CultureInfo.InvariantCulture,
+                        "Pinging search engine {0}. Response status code does not indicate success: {1} ({2}).",
+                        url,
+                        (int)response.StatusCode,
+                        response.ReasonPhrase));
+                    this.loggingService.Log(exception);
+                }
+            }
+#endif
+        }
+
+        #endregion
+    }
+}
