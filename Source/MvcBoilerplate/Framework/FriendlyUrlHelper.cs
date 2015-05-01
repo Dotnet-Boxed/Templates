d5b55dac952d2672b5e370a8a9eed16f23997f38
RehanSaeed <rehansaeed@gmail.com>
2015-05-01 18:57:06 +0100

    Version 1.0.39 - Fixed the 'Learn More' link to ASP.NET MVC Boilerplate in Index.cshtml. - Added a note about session state in Web.config file.

---
+++ Source/MvcBoilerplate/Framework/FriendlyUrlHelper.cs
@@ -0,0 +1,166 @@
+﻿namespace MvcBoilerplate.Framework
+{
+    using System.Globalization;
+    using System.Text;
+    using System.Web;
+
+    public static class FriendlyUrlHelper
+    {
+        public static string ToFriendlyUrl(string title, bool utf8 = true, int maxlength = 80)
+        {
+            if (title == null)
+            {
+                return string.Empty;
+            }
+
+            int length = title.Length;
+            bool prevdash = false;
+            StringBuilder stringBuilder = new StringBuilder(length);
+            char c;
+
+            for (int i = 0; i < length; ++i)
+            {
+                c = title[i];
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    stringBuilder.Append(c);
+                    prevdash = false;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    // tricky way to convert to lowercase
+                    stringBuilder.Append((char)(c | 32));
+                    prevdash = false;
+                }
+                else if ((c == ' ') || (c == ',') || (c == '.') || (c == '/') || (c == '\\') || (c == '-') || (c == '_') || (c == '='))
+                {
+                    if (!prevdash && (stringBuilder.Length > 0))
+                    {
+                        stringBuilder.Append('-');
+                        prevdash = true;
+                    }
+                }
+                else if (c >= 128)
+                {
+                    int previousLength = stringBuilder.Length;
+
+                    if (utf8)
+                    {
+                        //stringBuilder.Append(HttpUtility.UrlEncode(c.ToString(CultureInfo.InvariantCulture), Encoding.UTF8));
+                        stringBuilder.Append(c);
+                    }
+                    else
+                    {
+                        //http://meta.stackexchange.com/questions/7435/non-us-ascii-characters-dropped-from-full-profile-url/7696#7696
+                        stringBuilder.Append(RemapInternationalCharToAscii(c));
+                    }
+
+                    if (previousLength != stringBuilder.Length)
+                    {
+                        prevdash = false;
+                    }
+                }
+
+                if (i == maxlength)
+                {
+                    break;
+                }
+            }
+
+            if (prevdash)
+            {
+                return stringBuilder.ToString().Substring(0, stringBuilder.Length - 1);
+            }
+            else
+            {
+                return stringBuilder.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Remaps the international character  to ASCII.
+        /// </summary>
+        /// <param name="character">The character.</param>
+        /// <returns></returns>
+        private static string RemapInternationalCharToAscii(char character)
+        {
+            string s = character.ToString().ToLowerInvariant();
+            if ("àåáâäãåąā".Contains(s))
+            {
+                return "a";
+            }
+            else if ("èéêëę".Contains(s))
+            {
+                return "e";
+            }
+            else if ("ìíîïı".Contains(s))
+            {
+                return "i";
+            }
+            else if ("òóôõöøőð".Contains(s))
+            {
+                return "o";
+            }
+            else if ("ùúûüŭů".Contains(s))
+            {
+                return "u";
+            }
+            else if ("çćčĉ".Contains(s))
+            {
+                return "c";
+            }
+            else if ("żźž".Contains(s))
+            {
+                return "z";
+            }
+            else if ("śşšŝ".Contains(s))
+            {
+                return "s";
+            }
+            else if ("ñń".Contains(s))
+            {
+                return "n";
+            }
+            else if ("ýÿ".Contains(s))
+            {
+                return "y";
+            }
+            else if ("ğĝ".Contains(s))
+            {
+                return "g";
+            }
+            else if (character == 'ř')
+            {
+                return "r";
+            }
+            else if (character == 'ł')
+            {
+                return "l";
+            }
+            else if (character == 'đ')
+            {
+                return "d";
+            }
+            else if (character == 'ß')
+            {
+                return "ss";
+            }
+            else if (character == 'Þ')
+            {
+                return "th";
+            }
+            else if (character == 'ĥ')
+            {
+                return "h";
+            }
+            else if (character == 'ĵ')
+            {
+                return "j";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
