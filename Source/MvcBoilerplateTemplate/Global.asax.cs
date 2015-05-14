97a931d0830749397c812bf68657fab54e55285e
RehanSaeed <rehansaeed@gmail.com>
2015-05-14 20:42:33 +0100

    Version 1.0.46 - Comment about Anti-Forgery Tokens and the name of the corresponding form input.

--- Source/MvcBoilerplateTemplate/Global.asax.cs
+++ Source/MvcBoilerplateTemplate/Global.asax.cs
@@ -51,7 +51,10 @@
         {
             // Rename the Anti-Forgery cookie from "__RequestVerificationToken" to "f". 
             // This adds a little security through obscurity and also saves sending a 
-            // few characters over the wire.
+            // few characters over the wire. Sadly there is no way to change the form input
+            // name which is hard coded in the @Html.AntiForgeryToken helper and the 
+            // ValidationAntiforgeryTokenAttribute to  __RequestVerificationToken.
+            // <input name="__RequestVerificationToken" type="hidden" value="..." />
             AntiForgeryConfig.CookieName = "f";
 
             // If you have enabled SSL. Uncomment this line to ensure that the Anti-Forgery 
