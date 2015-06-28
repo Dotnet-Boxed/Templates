0fd9b7e4e5cd0025e5f444cde9f8ed5ed9be4ccc
RehanSaeed <rehansaeed@gmail.com>
2015-06-28 14:19:45 +0100

    MVC 6 - CSS, JS, Images and Fonts using NPM, Bower and Gulp

--- Source/MVC6/Boilerplate.Web.Mvc6.Sample/gulpfile.js
+++ Source/MVC6/Boilerplate.Web.Mvc6.Sample/gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require("gulp"),
     imagemin = require("gulp-imagemin"),        // https://www.npmjs.com/package/gulp-imagemin
     less = require("gulp-less"),                // https://www.npmjs.com/package/gulp-less
     minifyCss = require("gulp-minify-css"),     // https://www.npmjs.com/package/gulp-minify-css
-    sass = require("gulp-sass"),                // 
+    rename = require('gulp-rename'),            // https://www.npmjs.com/package/gulp-rename
     size = require("gulp-size"),                // https://www.npmjs.com/package/gulp-size
     sourcemaps = require('gulp-sourcemaps'),    // https://www.npmjs.com/package/gulp-sourcemaps
     svgmin = require("gulp-svgmin"),            // https://www.npmjs.com/package/gulp-svgmin
@@ -17,122 +17,116 @@ var gulp = require("gulp"),
 eval("var project = " + fs.readFileSync("./project.json"));
 
 var paths = {
+    // Source Folder Paths
     bower: "./bower_components/",
+    content: "Content/",
+    scripts: "Scripts/",
+
+    // Destination Folder Paths
     wwwroot: "./" + project.webroot,
-    lib: "./" + project.webroot + "/lib/",
-    css: "./" + project.webroot + "/css2/",
+    css: "./" + project.webroot + "/css/",
+    fonts: "./" + project.webroot + "/fonts/",
     img: "./" + project.webroot + "/img/",
     js: "./" + project.webroot + "/js/"
 };
 
 gulp.task("clean", function (cb) {
-    rimraf(paths.lib, cb);
-    //rimraf(paths.css, cb);
-    //rimraf(paths.js, cb);
+    rimraf(paths.css, function () { });
+    rimraf(paths.fonts, function () { });
+    rimraf(paths.js, cb);
 });
 
-gulp.task("copy", ["clean"], function () {
-    var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
-        "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
-        "hammer.js": "hammer.js/hammer*.{js,map}",
-        "jquery": "jquery/jquery*.{js,map}",
-        "jquery-validation": "jquery-validation/jquery.validate.js",
-        "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
-    }
+gulp.task("build-css", [], function () {
+    var allDirectories = [
+        paths.content + "bootstrap.less",
+        paths.bower + "bootstrap-touch-carousel/dist/**/*.{less}",
+        paths.content + "Site.less"
+    ];
+    gulp.src(allDirectories)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('all.css'))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(paths.css))
+        .pipe(size());
 
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir])
-          .pipe(gulp.dest(paths.lib + destinationDir));
-    }
+    var fontAwesomeDirectory = paths.bower + "font-awesome-less/less/font-awesome.less";
+    gulp.src(fontAwesomeDirectory)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('font-awesome.css'))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(paths.css))
+        .pipe(size());
 });
 
-gulp.task("copy-fonts", [], function () {
-    var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{ttf,svg,woff,eot}",
-    }
-
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir])
-          .pipe(gulp.dest(paths.css + destinationDir));
-    }
-});
-
-gulp.task("copy-js", ["clean"], function () {
-    var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{js,map}",
-        "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js}",
-        "hammer.js": "hammer.js/hammer*.{js,map}",
-        "jquery": "jquery/jquery*.{js,map}",
-        "jquery-validation": "jquery-validation/jquery.validate.js",
-        "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
-    }
-
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir])
-          .pipe(gulp.dest(paths.js + destinationDir));
-    }
-});
-
-gulp.task("optimize-css", [], function () {
-    var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{map,css,less}",
-        "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{css,less}"
-    }
-
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir])
-            .pipe(sourcemaps.init({ loadMaps: true })) // Initialize the source maps, also load existing maps.
-            .pipe(less({
-                paths: [path.join(__dirname, 'less', 'includes')]
+gulp.task("build-fonts", [], function () {
+    var directories = {
+        "bootstrap": "bootstrap-less/**/*.{ttf,svg,woff,woff2,otf,eot}",
+        "font-awesome": "font-awesome-less/**/*.{ttf,svg,woff,woff2,otf,eot}"
+    };
+    for (var directory in directories) {
+        gulp.src(paths.bower + directories[directory])
+            .pipe(rename(function (path) {
+                path.dirname = "";
             }))
-            .pipe(sourcemaps.write('/'))
-            .pipe(concat('all.css'))
-            .pipe(minifyCss())
-            .pipe(gulp.dest(paths.css))
-            .pipe(size());
+            .pipe(gulp.dest(paths.fonts + directory));
     }
 });
 
-gulp.task("optimize-js", [], function () {
-    var directories = {
-        "root": "**/*.{js,map}",
-    }
+gulp.task("build-js", [], function () {
+    var bootstrapDirectory = "bootstrap-less/js/*.js";
+    gulp.src(paths.bower + bootstrapDirectory)
+        .pipe(sourcemaps.init())
+        .pipe(concat("bootstrap.js"))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(paths.js + "bootstrap"))
+        .pipe(size());
 
-    for (var destinationDir in directories) {
-        gulp.src(paths.js + directories[destinationDir])
+    var directories = {
+        "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/js/bootstrap-touch-carousel.js",
+        "hammer": "hammer.js/hammer.js",
+        "jquery": "jquery/dist/jquery.js",
+        "jquery-validation": "jquery-validation/dist/jquery.validate.js",
+        "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
+    }
+    for (var directory in directories) {
+        gulp.src(paths.bower + directories[directory])
+            .pipe(sourcemaps.init())
             .pipe(uglify())
-            .pipe(gulp.dest(paths.js))
+            .pipe(sourcemaps.write('/'))
+            .pipe(gulp.dest(paths.js + directory))
             .pipe(size());
     }
 });
 
-gulp.task("optimize-img", [], function () {
-    var directories = {
-        "root": "**/*.{png,jpg,jpeg,gif,svg}",
-    }
+gulp.task("build", ["clean", "build-css", "build-fonts", "build-js"], function () { });
 
-    for (var destinationDir in directories) {
-        gulp.src(paths.img + directories[destinationDir])
-            .pipe(imagemin())
-            .pipe(gulp.dest(paths.img))
-            .pipe(size());
-    }
+gulp.task("compress-images", [], function () {
+    var directories = [
+        paths.img + "**/*.{png,jpg,jpeg,gif,svg}"
+    ];
+
+    gulp.src(directories)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.img))
+        .pipe(size());
 });
-
-gulp.task("build", ["clean", "copy-fonts", "copy-js", "optimize-css", "optimize-js"], function () { });
 
 
 // watch
 //var gulp = require('gulp'),
 //    watch = require('gulp-watch');
- 
+
 //gulp.task('stream', function () {
 //    gulp.src('css/**/*.css')
 //        .pipe(watch('css/**/*.css'))
 //        .pipe(gulp.dest('build'));
 //});
- 
+
 //gulp.task('callback', function () {
 //    watch('css/**/*.css', function () {
 //        gulp.src('css/**/*.css')
