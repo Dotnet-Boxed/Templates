81c9b8e4c951b109be24ba51cd50f2c1aad9b4da
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2015-10-28 10:09:12 +0000

    MVC 6 - Added UseCookiePolicy middleware to set better more secure cookie defaults.

--- Source/MVC6/Boilerplate.Web.Mvc6.Sample/Startup.Cookies.cs
+++ Source/MVC6/Boilerplate.Web.Mvc6.Sample/Startup.Cookies.cs
@@ -17,6 +17,9 @@
             IHostingEnvironment environment)
         {
             // $Start-HttpsEverywhere-On$
+            // In the Development environment, different ports are being used for HTTP and HTTPS. The 
+            // RequireHttpsAttribute expects to use the default ports 80 for HTTP and port 443 for HTTPS and simply
+            // adds an 's' onto 'http'. Therefore, we don't require secure cookies in the development environment.
             SecurePolicy securePolicy;
             if (environment.IsDevelopment())
             {
