14ff9ba1fd5d56d5960f864285adf0d74deab7fa
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2015-12-20 13:15:46 +0000

    Version 2.9.0 MVC 6 - Fixed CSP for Application Insights

--- Source/MVC6/Boilerplate.Web.Mvc6.Sample/Startup.ContentSecurityPolicy.cs
+++ Source/MVC6/Boilerplate.Web.Mvc6.Sample/Startup.ContentSecurityPolicy.cs
@@ -115,8 +115,14 @@
             filters.Add(
                 new CspConnectSrcAttribute()
                 {
-                    // Allow AJAX and Web Sockets to example.com.
-                    // CustomSources = "*.example.com",
+                    // Allow AJAX and Web Sockets to the following sources.
+                    CustomSources = string.Join(
+                        " ",
+                        // $Start-ApplicationInsights$
+                        "dc.services.visualstudio.com"          // Allow posting data back to Application Insights.
+                        // $End-ApplicationInsights$
+                        // CustomSources = "*.example.com",     // Allow AJAX and Web Sockets to example.com.
+                        ),
                     // Allow all AJAX and Web Sockets calls from the same domain.
                     Self = true
                 });
@@ -178,6 +184,9 @@
                     // Allow scripts from the CDN's.
                     CustomSources = string.Join(
                         " ",
+                        // $Start-ApplicationInsights$
+                        "az416426.vo.msecnd.net",   // Allow Application Insights to run scripts.
+                        // $End-ApplicationInsights$
                         ContentDeliveryNetwork.Google.Domain,
                         ContentDeliveryNetwork.Microsoft.Domain),
                     // Allow scripts from the same domain.
