4445fc96aa9d12730d36d40e608720111caa6e10
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-02-06 12:54:18 +0000

    Version 2.9.5 MVC 5 & 6 - Updated Newtonsoft.Json to 8.0.2. - Updated jQuery to 2.2.0. - Updated Bootstrap 3.3.6. MVC 5 - Switched from Twitter.Bootstrap.Less NuGet package to Bootstrap.Less.

--- Source/MVC5/Boilerplate.Web.Mvc5.Sample/Constants/ContentDeliveryNetwork.cs
+++ Source/MVC5/Boilerplate.Web.Mvc5.Sample/Constants/ContentDeliveryNetwork.cs
@@ -5,7 +5,7 @@
         public static class Google
         {
             public const string Domain = "ajax.googleapis.com";
-            public const string JQueryUrl = "//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+            public const string JQueryUrl = "//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
         }
 
         public static class MaxCdn
@@ -20,7 +20,7 @@
             public const string JQueryValidateUrl = "//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js";
             public const string JQueryValidateUnobtrusiveUrl = "//ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js";
             public const string ModernizrUrl = "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.8.3.js";
-            public const string BootstrapUrl = "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.5/bootstrap.min.js";
+            public const string BootstrapUrl = "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js";
         }
     }
 }
