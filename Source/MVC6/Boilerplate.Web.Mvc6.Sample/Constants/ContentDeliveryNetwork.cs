063977e803f4fea50faa8f7dfcc5041fe6e86f62
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-03-05 09:29:42 +0000

    MVC 6 - Packages file path in the .xproj is now corrected automatically with the update to the latest version of TemplateBuilder and the new Wizard I added to it.

--- Source/MVC6/Boilerplate.Web.Mvc6.Sample/Constants/ContentDeliveryNetwork.cs
+++ Source/MVC6/Boilerplate.Web.Mvc6.Sample/Constants/ContentDeliveryNetwork.cs
@@ -5,22 +5,22 @@
         public static class Google
         {
             public const string Domain = "ajax.googleapis.com";
-            public const string JQueryUrl = "//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+            public const string JQueryUrl = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
         }
 
         public static class MaxCdn
         {
             public const string Domain = "maxcdn.bootstrapcdn.com";
-            public const string FontAwesomeUrl = "//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css";
+            public const string FontAwesomeUrl = "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css";
         }
 
         public static class Microsoft
         {
             public const string Domain = "ajax.aspnetcdn.com";
-            public const string JQueryValidateUrl = "//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js";
-            public const string JQueryValidateUnobtrusiveUrl = "//ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js";
-            public const string ModernizrUrl = "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.8.3.js";
-            public const string BootstrapUrl = "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js";
+            public const string JQueryValidateUrl = "https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js";
+            public const string JQueryValidateUnobtrusiveUrl = "https://ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js";
+            public const string ModernizrUrl = "https://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.8.3.js";
+            public const string BootstrapUrl = "https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js";
         }
     }
 }
