87340c8e95d4071b80dcfb3c6ec3402413b23f8a
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-03-11 17:07:37 +0000

    Removed comment.

--- Source/MVC5/Boilerplate.Web.Mvc5.Sample/Scripts/Fallback/scripts.js
+++ Source/MVC5/Boilerplate.Web.Mvc5.Sample/Scripts/Fallback/scripts.js
@@ -1,16 +1,12 @@
-﻿// Performs tests to see if a script was loaded successfully, if the script failed to load, appends a new script tag 
+﻿// Performs tests to see if a script was loaded successfully, if the script failed to load, appends a new script tag
 // pointing to the local copy of the script and then waits for it to load before performing the next check.
-// Example: Bootstrap is dependant on jQuery. If loading jQuery from the CDN fails, this script loads the jQuery 
+// Example: Bootstrap is dependant on jQuery. If loading jQuery from the CDN fails, this script loads the jQuery
 //          fallback and waits for it to finish loading before attempting the next fallback test.
-// Note: You often see fallback scripts like this:
-//       <script>window.jQuery || document.write('<script src="/js/jquery.js"><\/script>');</script>
-//       The downside to this is that Bootstrap depends on jQuery and even if the above fallback script is added, 
-//       the Bootstrap script will error because the Bootstrap script will run before the jQuery fallback has loaded.
 (function (document) {
     "use strict";
 
     var fallbacks = [
-        // test - Tests whether the script loaded successfully or not. Returns true if the script loaded successfully or 
+        // test - Tests whether the script loaded successfully or not. Returns true if the script loaded successfully or
         //        false if the script failed to load and the fallback is required.
         // src - The URL to the fallback script.
         { test: function () { return window.Modernizr; }, src: "/bundles/modernizr" },
--- Source/MVC5/Boilerplate.Web.Mvc5.Sample/Scripts/Fallback/styles.js
+++ Source/MVC5/Boilerplate.Web.Mvc5.Sample/Scripts/Fallback/styles.js
@@ -1,4 +1,4 @@
-﻿// Performs tests to see if a stylesheet was loaded successfully, if the stylesheet failed to load, appends a new 
+﻿// Performs tests to see if a stylesheet was loaded successfully, if the stylesheet failed to load, appends a new
 // link tag pointing to the local copy of the stylesheet before performing the next check.
 (function (document) {
     "use strict";
@@ -6,11 +6,11 @@
     var fallbacks = [
         {
             // metaName - The name of the meta tag that the test is performed on. The meta tag must have a class from the
-            //            relevant stylesheet on it so it is styled and a test can be performed against it. E.g. for 
+            //            relevant stylesheet on it so it is styled and a test can be performed against it. E.g. for
             //            font awesome the <meta name="x-font-awesome-stylesheet-fallback-test" class="fa"> meta tag is
             //            added. The 'fa' class causes the font awesome style to be applied to it.
             metaName: "x-font-awesome-stylesheet-fallback-test",
-            // test - The test to perform against the meta tag. Checks to see if the Font awesome styles loaded 
+            // test - The test to perform against the meta tag. Checks to see if the Font awesome styles loaded
             //        successfully by checking that the font-family of the meta tag is 'FontAwesome'.
             test: function (meta) { return meta.style.fontFamily === "FontAwesome"; },
             // href - The URL to the fallback stylesheet.
@@ -37,5 +37,5 @@
         }
 
     }
-    
+
 })(document);
