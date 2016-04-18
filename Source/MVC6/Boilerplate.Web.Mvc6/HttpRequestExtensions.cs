b9ddeb488d57321515cad219da0f9ef46cc73538
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-04-18 08:42:40 +0100

    Version 2.16.0 MVC 6 - Added HttpRequestExtensions IsLocalRequest extension method.

--- Source/MVC6/Boilerplate.Web.Mvc6/HttpRequestExtensions.cs
+++ Source/MVC6/Boilerplate.Web.Mvc6/HttpRequestExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Boilerplate.Web.Mvc
 {
     using System;
+    using System.Net;
     using Microsoft.AspNet.Http;
 
     /// <summary>
@@ -27,6 +28,42 @@
             if (request.Headers != null)
             {
                 return request.Headers[RequestedWithHeader] == XmlHttpRequest;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the specified HTTP request is a local request where the IP address of the request
+        /// originator was 127.0.0.1.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns><c>true</c> if the specified HTTP request is a local request; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="request"/> parameter is <c>null</c>.</exception>
+        public static bool IsLocalRequest(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var connection = request.HttpContext.Connection;
+            if (connection.RemoteIpAddress != null)
+            {
+                if (connection.LocalIpAddress != null)
+                {
+                    return connection.RemoteIpAddress.Equals(connection.LocalIpAddress);
+                }
+                else
+                {
+                    return IPAddress.IsLoopback(connection.RemoteIpAddress);
+                }
+            }
+
+            // for in memory TestServer or when dealing with default connection info
+            if (connection.RemoteIpAddress == null && connection.LocalIpAddress == null)
+            {
+                return true;
             }
 
             return false;
