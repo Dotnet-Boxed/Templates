dcb30a0a4fac55f1c3fe440a9b169e5bed272f39
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-04-21 12:13:00 +0100

    Commented ValidateModelStateAttribute

--- Source/MVC6/Boilerplate.Web.Mvc6/Filters/ValidateModelStateAttribute.cs
+++ Source/MVC6/Boilerplate.Web.Mvc6/Filters/ValidateModelStateAttribute.cs
@@ -1,11 +1,20 @@
 ﻿namespace Boilerplate.Web.Mvc.Filters
 {
+    using System.Linq;
     using Microsoft.AspNet.Mvc;
     using Microsoft.AspNet.Mvc.Filters;
-    using System.Linq;
 
+    /// <summary>
+    /// Validates the action's arguments and model state. If any of the arguments are <c>null</c> or the model state is
+    /// invalid, returns a 400 Bad Request result.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNet.Mvc.Filters.ActionFilterAttribute" />
     public class ValidateModelStateAttribute : ActionFilterAttribute
     {
+        /// <summary>
+        /// Called when the action is executing.
+        /// </summary>
+        /// <param name="actionContext">The action context.</param>
         public override void OnActionExecuting(ActionExecutingContext actionContext)
         {
             if (actionContext.ActionArguments.Any(x => x.Value == null))
