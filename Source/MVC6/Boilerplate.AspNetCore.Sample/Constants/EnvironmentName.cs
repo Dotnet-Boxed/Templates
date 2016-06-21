a120d00152854293d97e0436904b68daa001be79
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-06-21 17:20:58 +0100

    EnvironmentName constant removed as now provided by RC2.

--- Source/MVC6/Boilerplate.AspNetCore.Sample/Constants/EnvironmentName.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcBoilerplate.Constants
-{
-    public static class EnvironmentName
-    {
-        public const string Development = "Development";
-        public const string Staging = "Staging";
-        public const string Production = "Production";
-    }
-}
