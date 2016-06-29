42712468fbf939b086786b85c77f63a73d426001
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-06-29 12:27:45 +0100

    Added IConfiguration extension method to GetSection with generic type parameter.

---
+++ Source/MVC6/Boilerplate.AspNetCore/ConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Boilerplate.AspNetCore
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// <see cref="IConfiguration"/>
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Gets a configuration sub-section with the specified key and binds it with the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the configuration section to bind to.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="key">The section key. If <c>null</c>, the name of the type <typeparamref name="T"/> is used.</param>
+        /// <returns>The bound object.</returns>
+        public static T GetSection<T>(this IConfiguration configuration, string key = null)
+            where T : new()
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            if (key == null)
+            {
+                key = typeof(T).Name;
+            }
+
+            var section = new T();
+            configuration.GetSection(key).Bind(section);
+            return section;
+        }
+    }
+}
