90bc23dfc662a3c851bf83c4a23d7f1611282c07
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-12-28 11:22:36 +0000

    Initial version of new dotnet new project template.

---
+++ Source/Boilerplate.ProjectTemplates/content/Boilerplate.AspNetCore.Template/Constants/ControllerName.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcBoilerplate.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}
---
+++ Source/Boilerplate.ProjectTemplates/content/Boilerplate.AspNetCore.Template/Constants/CorsPolicyName.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcBoilerplate.Constants
+{
+    public static class CorsPolicyName
+    {
+        public const string AllowAny = nameof(AllowAny);
+    }
+}
