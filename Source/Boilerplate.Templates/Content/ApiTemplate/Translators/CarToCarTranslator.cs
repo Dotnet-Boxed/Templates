a8bf551e2d58a81c6bd578af6e254fab8b9f5140
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2017-04-10 16:30:18 +0100

    Remove unused translator

--- Source/Boilerplate.Templates/Content/ApiTemplate/Translators/CarToCarTranslator.cs
+++ Source/Boilerplate.Templates/Content/ApiTemplate/Translators/CarToCarTranslator.cs
@@ -3,17 +3,9 @@
     using Boilerplate;
     using ApiTemplate.ViewModels;
 
-    public class CarToCarTranslator : ITranslator<Models.Car, Car>, ITranslator<Car, Models.Car>
+    public class CarToCarTranslator : ITranslator<Models.Car, Car>
     {
         public void Translate(Models.Car source, Car destination)
-        {
-            destination.CarId = source.CarId;
-            destination.Cylinders = source.Cylinders;
-            destination.Make = source.Make;
-            destination.Model = source.Model;
-        }
-
-        public void Translate(Car source, Models.Car destination)
         {
             destination.CarId = source.CarId;
             destination.Cylinders = source.Cylinders;
