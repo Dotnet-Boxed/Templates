5d3328c1473151985a3fda355bb91d819443d7ac
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-03-06 14:58:30 +0000

    Simplify DotnetNewInstall

--- Tests/Boilerplate.Templates.Test/ApiTemplateTest.cs
+++ Tests/Boilerplate.Templates.Test/ApiTemplateTest.cs
@@ -1,21 +1,16 @@
 namespace Boilerplate.Templates.Test
 {
-    using System.Net;
     using System.Reflection;
     using System.Threading.Tasks;
     using Xunit;
 
     public class ApiTemplateTest
     {
-        private const string ProjectFileName = "ApiTemplate.csproj";
-        private const string TemplateName = "api";
-
         public ApiTemplateTest()
         {
-            var projectDirectoryPath = TemplateAssert.GetProjectDirectoryPath(
+            TemplateAssert.DotnetNewInstall(
                 typeof(ApiTemplateTest).GetTypeInfo().Assembly,
-                ProjectFileName);
-            TemplateAssert.DotnetNewInstall(projectDirectoryPath).Wait();
+                "ApiTemplate.csproj").Wait();
         }
 
         [Fact]
