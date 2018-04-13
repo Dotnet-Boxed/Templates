138e6e013766a8298f3a4d9a9adf0bce513f6363
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-04-13 10:45:53 +0100

    Remove health check

--- Source/Boilerplate.Templates/Content/GraphQLTemplate/Constants/ControllerName.cs
+++
@@ -1,9 +0,0 @@
-namespace ApiTemplate.Constants
-{
-    public static class ControllerName
-    {
-#if (HealthCheck)
-        public const string Status = nameof(Status);
-#endif
-    }
-}
--- Source/Boilerplate.Templates/Content/GraphQLTemplate/IHealthChecker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiTemplate
-{
-    using System.Threading.Tasks;
-
-    public interface IHealthChecker
-    {
-        Task CheckHealth();
-    }
-}
