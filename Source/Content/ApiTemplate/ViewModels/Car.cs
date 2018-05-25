0569c2ec1c32c06f5f86f1f9609eec64a8b06c9e
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-05-25 16:06:12 +0100

    Add Swagger comments

--- Source/Content/ApiTemplate/ViewModels/Car.cs
+++ Source/Content/ApiTemplate/ViewModels/Car.cs
@@ -4,18 +4,46 @@ namespace ApiTemplate.ViewModels
     using ApiTemplate.ViewModelSchemaFilters;
     using Swashbuckle.AspNetCore.SwaggerGen;
 
+    /// <summary>
+    /// A make and model of car.
+    /// </summary>
     [SwaggerSchemaFilter(typeof(CarSchemaFilter))]
 #endif
     public class Car
     {
+#if (Swagger)
+        /// <summary>
+        /// The cars unique identifier.
+        /// </summary>
+#endif
         public int CarId { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The number of cylinders in the cars engine.
+        /// </summary>
+#endif
         public int Cylinders { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The make of the car.
+        /// </summary>
+#endif
         public string Make { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The model of the car.
+        /// </summary>
+#endif
         public string Model { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The URL used to retrieve the resource conforming to REST'ful JSON http://restfuljson.org/.
+        /// </summary>
+#endif
         public string Url { get; set; }
     }
 }
--- Source/Content/ApiTemplate/ViewModels/SaveCar.cs
+++ Source/Content/ApiTemplate/ViewModels/SaveCar.cs
@@ -1,4 +1,4 @@
-﻿namespace ApiTemplate.ViewModels
+namespace ApiTemplate.ViewModels
 {
     using System.ComponentModel.DataAnnotations;
 #if (Swagger)
@@ -7,16 +7,34 @@
 #endif
 
 #if (Swagger)
+    /// <summary>
+    /// A make and model of car.
+    /// </summary>
     [SwaggerSchemaFilter(typeof(SaveCarSchemaFilter))]
 #endif
     public class SaveCar
     {
+#if (Swagger)
+        /// <summary>
+        /// The number of cylinders in the cars engine.
+        /// </summary>
+#endif
         [Range(1, 20)]
         public int Cylinders { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The make of the car.
+        /// </summary>
+#endif
         [Required]
         public string Make { get; set; }
 
+#if (Swagger)
+        /// <summary>
+        /// The model of the car.
+        /// </summary>
+#endif
         [Required]
         public string Model { get; set; }
     }
