f8737d3165c227ca87ae7b42dd0776a31620f0c7
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-06-23 19:23:45 +0100

    Create MvcCoreBuilderExtensions

--- Source/Content/ApiTemplate/CustomServiceCollectionExtensions.cs
+++ Source/Content/ApiTemplate/CustomServiceCollectionExtensions.cs
@@ -8,14 +8,10 @@ namespace ApiTemplate
 #if (Swagger)
     using System.Reflection;
 #endif
-#if (CORS)
-    using ApiTemplate.Constants;
-#endif
 #if (Swagger && Versioning)
     using ApiTemplate.OperationFilters;
 #endif
     using ApiTemplate.Options;
-    using Boxed.AspNetCore;
 #if (Swagger)
     using Boxed.AspNetCore.Swagger;
     using Boxed.AspNetCore.Swagger.OperationFilters;
@@ -28,11 +24,9 @@ namespace ApiTemplate
 #if (!ForwardedHeaders && HostFiltering)
     using Microsoft.AspNetCore.HostFiltering;
 #endif
-    using Microsoft.AspNetCore.Hosting;
 #if (Versioning)
     using Microsoft.AspNetCore.Mvc.ApiExplorer;
 #endif
-    using Microsoft.AspNetCore.Mvc.Formatters;
 #if (ResponseCompression)
     using Microsoft.AspNetCore.ResponseCompression;
 #endif
@@ -41,8 +35,6 @@ namespace ApiTemplate
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Options;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Converters;
 #if (Swagger)
     using Swashbuckle.AspNetCore.Swagger;
 #endif
@@ -181,84 +173,6 @@ namespace ApiTemplate
                 {
                     options.AssumeDefaultVersionWhenUnspecified = true;
                     options.ReportApiVersions = true;
-                });
-
-#endif
-        public static IMvcCoreBuilder AddCustomMvcOptions(
-            this IMvcCoreBuilder builder,
-            IHostingEnvironment hostingEnvironment) =>
-            builder.AddMvcOptions(
-                options =>
-                {
-                    // Controls how controller actions cache content from the appsettings.json file.
-                    var cacheProfileOptions = builder.Services.BuildServiceProvider().GetRequiredService<CacheProfileOptions>();
-                    foreach (var keyValuePair in cacheProfileOptions)
-                    {
-                        options.CacheProfiles.Add(keyValuePair);
-                    }
-
-                    var jsonInputFormatterMediaTypes = options
-                        .InputFormatters
-                        .OfType<JsonInputFormatter>()
-                        .First()
-                        .SupportedMediaTypes;
-                    var jsonOutputFormatterMediaTypes = options
-                        .OutputFormatters
-                        .OfType<JsonOutputFormatter>()
-                        .First()
-                        .SupportedMediaTypes;
-
-                    // Add RESTful JSON media type (application/vnd.restful+json) to the JSON input and output formatters.
-                    // See http://restfuljson.org/
-                    jsonInputFormatterMediaTypes.Insert(0, ContentType.RestfulJson);
-                    jsonOutputFormatterMediaTypes.Insert(0, ContentType.RestfulJson);
-
-                    // Remove string and stream output formatters. These are not useful for an API serving JSON or XML.
-                    options.OutputFormatters.RemoveType<StreamOutputFormatter>();
-                    options.OutputFormatters.RemoveType<StringOutputFormatter>();
-
-                    // Returns a 406 Not Acceptable if the MIME type in the Accept HTTP header is not valid.
-                    options.ReturnHttpNotAcceptable = true;
-                });
-
-        /// <summary>
-        /// Adds customized JSON serializer settings.
-        /// </summary>
-        public static IMvcCoreBuilder AddCustomJsonOptions(this IMvcCoreBuilder builder, IHostingEnvironment hostingEnvironment) =>
-            builder.AddJsonOptions(
-                options =>
-                {
-                    if (hostingEnvironment.IsDevelopment())
-                    {
-                        // Pretty print the JSON in development for easier debugging.
-                        options.SerializerSettings.Formatting = Formatting.Indented;
-                    }
-
-                    // Parse dates as DateTimeOffset values by default. You should prefer using DateTimeOffset over
-                    // DateTime everywhere. Not doing so can cause problems with time-zones.
-                    options.SerializerSettings.DateParseHandling = DateParseHandling.DateTimeOffset;
-
-                    // Output enumeration values as strings in JSON.
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                });
-
-#if (CORS)
-        /// <summary>
-        /// Add cross-origin resource sharing (CORS) services and configures named CORS policies. See
-        /// https://docs.asp.net/en/latest/security/cors.html
-        /// </summary>
-        public static IMvcCoreBuilder AddCustomCors(this IMvcCoreBuilder builder) =>
-            builder.AddCors(
-                options =>
-                {
-                    // Create named CORS policies here which you can consume using application.UseCors("PolicyName")
-                    // or a [EnableCors("PolicyName")] attribute on your controller or action.
-                    options.AddPolicy(
-                        CorsPolicyName.AllowAny,
-                        x => x
-                            .AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader());
                 });
 
 #endif
