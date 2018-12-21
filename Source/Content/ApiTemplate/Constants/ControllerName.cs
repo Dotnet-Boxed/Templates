ea86fc1a440348541c6f3eab4d781eb0cd6624ef
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-12-21 16:04:04 +0000

    Remove StatusController

--- Source/Content/ApiTemplate/Constants/ControllerName.cs
+++ Source/Content/ApiTemplate/Constants/ControllerName.cs
@@ -3,8 +3,5 @@ namespace ApiTemplate.Constants
     public static class ControllerName
     {
         public const string Car = nameof(Car);
-#if (HealthCheck)
-        public const string Status = nameof(Status);
-#endif
     }
 }
--- Source/Content/ApiTemplate/Constants/StatusControllerRoute.cs
+++
@@ -1,7 +0,0 @@
-namespace ApiTemplate.Constants
-{
-    public static class StatusControllerRoute
-    {
-        public const string GetStatus = ControllerName.Status + nameof(GetStatus);
-    }
-}
