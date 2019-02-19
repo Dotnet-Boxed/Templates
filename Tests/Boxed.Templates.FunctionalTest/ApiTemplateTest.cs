b1f20a058825fe68e6ce4ed3af94815d8df5c196
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2019-02-19 14:21:19 +0000

    Add more assertions

--- Tests/Boxed.Templates.FunctionalTest/ApiTemplateTest.cs
+++ Tests/Boxed.Templates.FunctionalTest/ApiTemplateTest.cs
@@ -4,7 +4,11 @@ namespace Boxed.Templates.FunctionalTest
     using System.Collections.Generic;
     using System.Linq;
     using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Threading.Tasks;
+    using ApiTemplate.ViewModels;
+    using Boxed.AspNetCore;
     using Xunit;
 
     public class ApiTemplateTest
@@ -46,24 +50,40 @@ namespace Boxed.Templates.FunctionalTest
 
                         var statusResponse = await httpsClient.GetAsync("status");
                         Assert.Equal(HttpStatusCode.OK, statusResponse.StatusCode);
+                        Assert.Equal(ContentType.Text, statusResponse.Content.Headers.ContentType.MediaType);
 
                         var statusSelfResponse = await httpsClient.GetAsync("status/self");
                         Assert.Equal(HttpStatusCode.OK, statusSelfResponse.StatusCode);
+                        Assert.Equal(ContentType.Text, statusSelfResponse.Content.Headers.ContentType.MediaType);
 
-                        var carsResponse = await httpsClient.GetAsync("/cars");
+                        var carsResponse = await httpsClient.GetAsync("cars");
                         Assert.Equal(HttpStatusCode.OK, carsResponse.StatusCode);
+                        Assert.Equal(ContentType.RestfulJson, carsResponse.Content.Headers.ContentType.MediaType);
+
+                        var postCarResponse = await httpsClient.PostAsJsonAsync("cars", new SaveCar());
+                        Assert.Equal(HttpStatusCode.BadRequest, postCarResponse.StatusCode);
+                        Assert.Equal(ContentType.ProblemJson, postCarResponse.Content.Headers.ContentType.MediaType);
+
+                        var notAcceptableCarsRequest = new HttpRequestMessage(HttpMethod.Get, "cars");
+                        notAcceptableCarsRequest.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(ContentType.Text));
+                        var notAcceptableCarsResponse = await httpsClient.SendAsync(notAcceptableCarsRequest);
+                        Assert.Equal(HttpStatusCode.NotAcceptable, notAcceptableCarsResponse.StatusCode);
 
                         var swaggerJsonResponse = await httpsClient.GetAsync("swagger/v1/swagger.json");
                         Assert.Equal(HttpStatusCode.OK, swaggerJsonResponse.StatusCode);
+                        Assert.Equal(ContentType.Json, swaggerJsonResponse.Content.Headers.ContentType.MediaType);
 
                         var robotsTxtResponse = await httpsClient.GetAsync("robots.txt");
                         Assert.Equal(HttpStatusCode.OK, robotsTxtResponse.StatusCode);
+                        Assert.Equal(ContentType.Text, robotsTxtResponse.Content.Headers.ContentType.MediaType);
 
                         var securityTxtResponse = await httpsClient.GetAsync(".well-known/security.txt");
                         Assert.Equal(HttpStatusCode.OK, securityTxtResponse.StatusCode);
+                        Assert.Equal(ContentType.Text, securityTxtResponse.Content.Headers.ContentType.MediaType);
 
                         var humansTxtResponse = await httpsClient.GetAsync("humans.txt");
                         Assert.Equal(HttpStatusCode.OK, humansTxtResponse.StatusCode);
+                        Assert.Equal(ContentType.Text, humansTxtResponse.Content.Headers.ContentType.MediaType);
                     });
             }
         }
