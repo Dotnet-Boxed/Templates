7386b320955932122c6750bf8ddf9956882db537
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2019-11-02 14:49:29 +0000

    Fix controller name

--- Source/ApiTemplate/Source/ApiTemplate/Constants/ControllerName.cs
+++ Source/ApiTemplate/Source/ApiTemplate/Constants/ControllerName.cs
@@ -2,6 +2,6 @@ namespace ApiTemplate.Constants
 {
     public static class ControllerName
     {
-        public const string Car = nameof(Car);
+        public const string Cars = nameof(Cars);
     }
 }
