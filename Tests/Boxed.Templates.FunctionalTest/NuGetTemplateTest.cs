d59ade63a85463056c35f807a30565a738c17d1c
Muhammad Rehan Saeed <musa@microsoft.com>
2020-01-27 13:45:44 +0000

    Add skipped test

--- Tests/Boxed.Templates.FunctionalTest/NuGetTemplateTest.cs
+++ Tests/Boxed.Templates.FunctionalTest/NuGetTemplateTest.cs
@@ -1,6 +1,7 @@
 namespace Boxed.Templates.FunctionalTest
 {
     using System;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using Boxed.DotnetNewTest;
@@ -39,6 +40,32 @@ namespace Boxed.Templates.FunctionalTest
                 var project = await tempDirectory.DotnetNewAsync(TemplateName, name, dictionary).ConfigureAwait(false);
                 await project.DotnetRestoreAsync().ConfigureAwait(false);
                 await project.DotnetBuildAsync().ConfigureAwait(false);
+            }
+        }
+
+        [Theory(Skip = "#if pre-processor statements don't work in .cake files. See https://github.com/dotnet/templating/issues/2243")]
+        [InlineData("NuGetNoAppVeyor", "appveyor.yml", "AppVeyor", "appveyor=false")]
+        [InlineData("NuGetNoAzurePipelines", "azure-pipelines.yml", "AzurePipelines", "azure-pipelines=false")]
+        [InlineData("NuGetNoGitHubActions", @"/.github/workflows/build.yml", "GitHubActions", "github-actions=false")]
+        public async Task RestoreAndBuild_NoContinuousIntegration_SuccessfulAsync(
+            string name,
+            string relativeFilePath,
+            string cakeContent,
+            params string[] arguments)
+        {
+            await InstallTemplateAsync().ConfigureAwait(false);
+            using (var tempDirectory = TempDirectory.NewTempDirectory())
+            {
+                var dictionary = arguments
+                    .Select(x => x.Split('=', StringSplitOptions.RemoveEmptyEntries))
+                    .ToDictionary(x => x.First(), x => x.Last());
+                var project = await tempDirectory.DotnetNewAsync(TemplateName, name, dictionary).ConfigureAwait(false);
+                await project.DotnetRestoreAsync().ConfigureAwait(false);
+                await project.DotnetBuildAsync().ConfigureAwait(false);
+
+                Assert.False(File.Exists(Path.Combine(project.DirectoryPath, relativeFilePath)));
+                var cake = await File.ReadAllTextAsync(Path.Combine(project.DirectoryPath, "build.cake")).ConfigureAwait(false);
+                Assert.DoesNotContain(cakeContent, cake, StringComparison.Ordinal);
             }
         }
 
