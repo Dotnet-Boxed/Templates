5dfbe4af8e522a09503ccf01709d934e166c0b4b
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-07-30 11:42:11 +0100

    Add paging

--- Source/GraphQLTemplate/Source/GraphQLTemplate/Schemas/QueryObject.cs
+++ Source/GraphQLTemplate/Source/GraphQLTemplate/Schemas/QueryObject.cs
@@ -1,13 +1,11 @@
 namespace GraphQLTemplate.Schemas
 {
     using System;
-    using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
     using GraphQLTemplate.Models;
-    using GraphQLTemplate.Repositories;
     using GraphQLTemplate.Resolvers;
+    using GraphQLTemplate.Types;
     using HotChocolate.Types;
+    using HotChocolate.Types.Relay;
 
     /// <summary>
     /// All queries defined in the schema used to retrieve data.
@@ -16,27 +14,29 @@ namespace GraphQLTemplate.Schemas
     /// The is an example query to get a human and the details of their friends.
     /// <c>
     /// query getHuman {
-    ///   human(id: "94fbd693-2027-4804-bf40-ed427fe76fda") {
+    ///   node(id: "SHVtYW4KZzk0ZmJkNjkzMjAyNzQ4MDRiZjQwZWQ0MjdmZTc2ZmRh") {
     ///     id
-    ///     name
-    ///     dateOfBirth
-    ///     homePlanet
-    ///     appearsIn
-    ///     friends {
-    ///       id
+    ///     ... on Human {
     ///       name
-    ///       ... on Droid {
-    ///         primaryFunction
-    ///         manufactured
-    ///         chargePeriod
+    ///       dateOfBirth
+    ///       homePlanet
+    ///       appearsIn
+    ///       friends {
+    ///         id
+    ///         name
+    ///         ... on Droid {
+    ///           primaryFunction
+    ///           manufactured
+    ///           chargePeriod
+    ///         }
+    ///         ... on Human {
+    ///           dateOfBirth
+    ///           homePlanet
+    ///         }
     ///       }
-    ///       ... on Human {
-    ///         dateOfBirth
-    ///         homePlanet
-    ///       }
+    ///       created
+    ///       modified
     ///     }
-    ///     created
-    ///     modified
     ///   }
     /// }
     /// </c>
@@ -44,14 +44,6 @@ namespace GraphQLTemplate.Schemas
     /// </example>
     public class QueryObject : ObjectType<QueryResolvers>
     {
-        private const int MaxPageSize = 10;
-
-        public QueryObject()
-        {
-            this.Name = "Query";
-            this.Description = "The query type, represents all of the entry points into our object graph.";
-        }
-
         protected override void Configure(IObjectTypeDescriptor<QueryResolvers> descriptor)
         {
             if (descriptor is null)
@@ -59,110 +51,21 @@ namespace GraphQLTemplate.Schemas
                 throw new ArgumentNullException(nameof(descriptor));
             }
 
-            // descriptor.Connection<DroidObject>()
-            //     .Name("droids")
-            //     .Description("Gets pages of droids.")
-            //     // Enable the last and before arguments to do paging in reverse.
-            //     .Bidirectional()
-            //     // Set the maximum size of a page, use .ReturnAll() to set no maximum size.
-            //     .PageSize(MaxPageSize)
-            //     .ResolveAsync(context => ResolveConnectionAsync(droidRepository, context));
-        }
+            // this.Name = "Query";
+            // this.Description = "The query type, represents all of the entry points into our object graph.";
 
-        // private static async Task<object> ResolveConnectionAsync(
-        //     IDroidRepository droidRepository,
-        //     ResolveConnectionContext<object> context)
-        // {
-        //     var first = context.First;
-        //     var afterCursor = Cursor.FromCursor<DateTime?>(context.After);
-        //     var last = context.Last;
-        //     var beforeCursor = Cursor.FromCursor<DateTime?>(context.Before);
-        //     var cancellationToken = context.CancellationToken;
-        //
-        //     var getDroidsTask = GetDroidsAsync(droidRepository, first, afterCursor, last, beforeCursor, cancellationToken);
-        //     var getHasNextPageTask = GetHasNextPageAsync(droidRepository, first, afterCursor, cancellationToken);
-        //     var getHasPreviousPageTask = GetHasPreviousPageAsync(droidRepository, last, beforeCursor, cancellationToken);
-        //     var totalCountTask = droidRepository.GetTotalCountAsync(cancellationToken);
-        //
-        //     await Task.WhenAll(getDroidsTask, getHasNextPageTask, getHasPreviousPageTask, totalCountTask).ConfigureAwait(false);
-        //     var droids = await getDroidsTask.ConfigureAwait(false);
-        //     var hasNextPage = await getHasNextPageTask.ConfigureAwait(false);
-        //     var hasPreviousPage = await getHasPreviousPageTask.ConfigureAwait(false);
-        //     var totalCount = await totalCountTask.ConfigureAwait(false);
-        //     var (firstCursor, lastCursor) = Cursor.GetFirstAndLastCursor(droids, x => x.Manufactured);
-        //
-        //     return new Connection<Droid>()
-        //     {
-        //         Edges = droids
-        //             .Select(x =>
-        //                 new Edge<Droid>()
-        //                 {
-        //                     Cursor = Cursor.ToCursor(x.Manufactured),
-        //                     Node = x,
-        //                 })
-        //             .ToList(),
-        //         PageInfo = new PageInfo()
-        //         {
-        //             HasNextPage = hasNextPage,
-        //             HasPreviousPage = hasPreviousPage,
-        //             StartCursor = firstCursor,
-        //             EndCursor = lastCursor,
-        //         },
-        //         TotalCount = totalCount,
-        //     };
-        // }
-
-        private static Task<List<Droid>> GetDroidsAsync(
-            IDroidRepository droidRepository,
-            int? first,
-            DateTime? afterCursor,
-            int? last,
-            DateTime? beforeCursor,
-            CancellationToken cancellationToken)
-        {
-            Task<List<Droid>> getDroidsTask;
-            if (first.HasValue)
-            {
-                getDroidsTask = droidRepository.GetDroidsAsync(first, afterCursor, cancellationToken);
-            }
-            else
-            {
-                getDroidsTask = droidRepository.GetDroidsReverseAsync(last, beforeCursor, cancellationToken);
-            }
-
-            return getDroidsTask;
-        }
-
-        private static Task<bool> GetHasNextPageAsync(
-            IDroidRepository droidRepository,
-            int? first,
-            DateTime? afterCursor,
-            CancellationToken cancellationToken)
-        {
-            if (first.HasValue)
-            {
-                return droidRepository.GetHasNextPageAsync(first, afterCursor, cancellationToken);
-            }
-            else
-            {
-                return Task.FromResult(false);
-            }
-        }
-
-        private static Task<bool> GetHasPreviousPageAsync(
-            IDroidRepository droidRepository,
-            int? last,
-            DateTime? beforeCursor,
-            CancellationToken cancellationToken)
-        {
-            if (last.HasValue)
-            {
-                return droidRepository.GetHasPreviousPageAsync(last, beforeCursor, cancellationToken);
-            }
-            else
-            {
-                return Task.FromResult(false);
-            }
+            descriptor
+                .Field(x => x.GetDroidsAsync(default))
+                .Description("Gets droids.")
+                .UsePaging<DroidObject, Droid>()
+                .UseFiltering()
+                .UseSorting();
+            descriptor
+                .Field(x => x.GetHumansAsync(default))
+                .Description("Gets humans.")
+                .UsePaging<HumanObject, Human>()
+                .UseFiltering()
+                .UseSorting();
         }
     }
 }
