466c174346c07d791d526567a8f07b5a4ce2c11e
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-12-11 11:35:37 +0000

    Add Open Telemetry option to GraphQL template

--- Source/GraphQLTemplate/Source/GraphQLTemplate/CustomServiceCollectionExtensions.cs
+++ Source/GraphQLTemplate/Source/GraphQLTemplate/CustomServiceCollectionExtensions.cs
@@ -22,6 +22,10 @@ namespace GraphQLTemplate
     using Microsoft.AspNetCore.HostFiltering;
 #endif
     using Microsoft.AspNetCore.Hosting;
+#if OpenTelemetry
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.HttpOverrides;
+#endif
 #if ResponseCompression
     using Microsoft.AspNetCore.ResponseCompression;
 #endif
@@ -30,6 +34,9 @@ namespace GraphQLTemplate
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Options;
+#if OpenTelemetry
+    using OpenTelemetry.Trace;
+#endif
 
     /// <summary>
     /// <see cref="IServiceCollection"/> extension methods which extend ASP.NET Core services.
@@ -178,6 +185,69 @@ namespace GraphQLTemplate
                 .AddHealthChecks()
                 // Add health checks for external dependencies here. See https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
                 .Services;
+#endif
+#if OpenTelemetry
+
+        /// <summary>
+        /// Adds Open Telemetry services and configures instrumentation and exporters.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <returns>The services with open telemetry added.</returns>
+        public static IServiceCollection AddCustomOpenTelemetryTracing(this IServiceCollection services) =>
+            services.AddOpenTelemetryTracing(
+                builder =>
+                {
+                    builder
+                        .AddAspNetCoreInstrumentation(
+                            options =>
+                            {
+                                // Enrich spans with additional request and response meta data.
+                                // See https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/http.md
+                                options.Enrich = (activity, eventName, obj) =>
+                                {
+                                    if (obj is HttpRequest request)
+                                    {
+                                        activity.AddTag("http.flavor", GetHttpFlavour(request.Protocol));
+                                        activity.AddTag("http.scheme", request.Scheme);
+                                        activity.AddTag("http.client_ip", request.Headers[ForwardedHeadersDefaults.XForwardedForHeaderName]);
+                                        activity.AddTag("http.request_content_length", request.ContentLength);
+                                        activity.AddTag("http.request_content_type", request.ContentType);
+                                    }
+                                    else if (obj is HttpResponse response)
+                                    {
+                                        activity.AddTag("http.response_content_length", response.ContentLength);
+                                        activity.AddTag("http.response_content_type", response.ContentType);
+                                    }
+
+                                    static string GetHttpFlavour(string protocol)
+                                    {
+                                        if (HttpProtocol.IsHttp10(protocol))
+                                        {
+                                            return "1.0";
+                                        }
+                                        else if (HttpProtocol.IsHttp11(protocol))
+                                        {
+                                            return "1.1";
+                                        }
+                                        else if (HttpProtocol.IsHttp2(protocol))
+                                        {
+                                            return "2.0";
+                                        }
+                                        else if (HttpProtocol.IsHttp3(protocol))
+                                        {
+                                            return "3.0";
+                                        }
+
+                                        throw new InvalidOperationException($"Protocol {protocol} not recognised.");
+                                    }
+                                };
+                                options.RecordException = true;
+                            });
+                    // TODO: Add OpenTelemetry.Instrumentation.* NuGet packages and configure them to collect more span data.
+                    //       E.g. add OpenTelemetry.Instrumentation.Http to instrument calls to HttpClient.
+                    // TODO: Add OpenTelemetry.Exporter.* NuGet packages and configure them here to export open telemetry span data.
+                    //       E.g. Add OpenTelemetry.Exporter.Jaeger to export span data to Jaeger.
+                });
 #endif
 
         public static IServiceCollection AddCustomGraphQL(
