89d979bf4c71c7487c0b7d46414ebc82c41a200e
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-02-25 08:52:04 +0000

    Fix possible NullReferenceException

--- Source/GraphQLTemplate/Source/GraphQLTemplate/MvcBuilderExtensions.cs
+++ Source/GraphQLTemplate/Source/GraphQLTemplate/MvcBuilderExtensions.cs
@@ -36,11 +36,15 @@ namespace GraphQLTemplate
                 options =>
                 {
                     // Controls how controller actions cache content from the appsettings.json file.
-                    foreach (var keyValuePair in configuration
+                    var cacheProfileOptions = configuration
                         .GetSection(nameof(ApplicationOptions.CacheProfiles))
-                        .Get<CacheProfileOptions>())
+                        .Get<CacheProfileOptions>();
+                    if (cacheProfileOptions != null)
                     {
-                        options.CacheProfiles.Add(keyValuePair);
+                        foreach (var keyValuePair in cacheProfileOptions)
+                        {
+                            options.CacheProfiles.Add(keyValuePair);
+                        }
                     }
 
                     // Remove plain text (text/plain) output formatter.
