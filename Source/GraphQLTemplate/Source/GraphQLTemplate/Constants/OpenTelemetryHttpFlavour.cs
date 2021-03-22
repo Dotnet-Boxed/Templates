c4e086b1b580b11ab4d493e1ee25a20e363311e4
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-03-22 08:54:06 +0000

    Bump OpenTelemetry.* from 1.0.0-rc2 to 1.0.0-rc3

---
+++ Source/GraphQLTemplate/Source/GraphQLTemplate/Constants/OpenTelemetryHttpFlavour.cs
@@ -0,0 +1,10 @@
+namespace GraphQLTemplate.Constants
+{
+    public static class OpenTelemetryHttpFlavour
+    {
+        public const string Http10 = "1.0";
+        public const string Http11 = "1.1";
+        public const string Http20 = "2.0";
+        public const string Http30 = "3.0";
+    }
+}
