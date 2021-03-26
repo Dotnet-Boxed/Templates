4fac32cbcfb4a3eebb9bd8e717356e6d207a6028
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-03-26 11:35:24 +0000

    Add SDL test

---
+++ Source/GraphQLTemplate/Tests/GraphQLTemplate.IntegrationTest/GraphQLTest.cs
@@ -0,0 +1,27 @@
+namespace GraphQLTemplate.IntegrationTest.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class GraphQLTest : CustomWebApplicationFactory<Startup>
+    {
+        private readonly HttpClient client;
+
+        public GraphQLTest(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper) =>
+            this.client = this.CreateClient();
+
+        [Fact]
+        public async Task GetSchemaDescriptionLanguage_Default_Returns200OkAsync()
+        {
+            var response = await this.client.GetAsync("/graphql?sdl").ConfigureAwait(false);
+            var sdl = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEmpty(sdl);
+        }
+    }
+}
--- Source/GraphQLTemplate/Tests/GraphQLTemplate.IntegrationTest/Models/GraphQLErrorLocation.cs
+++
@@ -1,9 +0,0 @@
-namespace GraphQLTemplate.IntegrationTest.Models
-{
-    public class GraphQLErrorLocation
-    {
-        public int Line { get; set; }
-
-        public int Column { get; set; }
-    }
-}
