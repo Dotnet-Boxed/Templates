674a7ee02fccc8a902cfc5de1788175e0beee146
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-08-11 15:20:53 +0100

    Update JSON options

--- Source/GraphQLTemplate/Source/GraphQLTemplate/MvcBuilderExtensions.cs
+++ Source/GraphQLTemplate/Source/GraphQLTemplate/MvcBuilderExtensions.cs
@@ -1,36 +1,12 @@
 namespace GraphQLTemplate
 {
     using GraphQLTemplate.Options;
-    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc.Formatters;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Converters;
 
     public static class MvcBuilderExtensions
     {
-        public static IMvcBuilder AddCustomJsonOptions(
-            this IMvcBuilder builder,
-            IWebHostEnvironment webHostEnvironment) =>
-            builder.AddNewtonsoftJson(
-                options =>
-                {
-                    if (webHostEnvironment.IsDevelopment())
-                    {
-                        // Pretty print the JSON in development for easier debugging.
-                        options.SerializerSettings.Formatting = Formatting.Indented;
-                    }
-
-                    // Parse dates as DateTimeOffset values by default. You should prefer using DateTimeOffset over
-                    // DateTime everywhere. Not doing so can cause problems with time-zones.
-                    options.SerializerSettings.DateParseHandling = DateParseHandling.DateTimeOffset;
-
-                    // Output enumeration values as strings in JSON.
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                });
-
         public static IMvcBuilder AddCustomMvcOptions(this IMvcBuilder builder, IConfiguration configuration) =>
             builder.AddMvcOptions(
                 options =>
