6368b0586a65bc6eb47dcb1ec08ca19fba9db6fe
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-11-05 16:04:56 +0000

    Add comment

--- Source/ApiTemplate/Source/ApiTemplate/CustomJsonSerializerContext.cs
+++ Source/ApiTemplate/Source/ApiTemplate/CustomJsonSerializerContext.cs
@@ -3,6 +3,9 @@ namespace ApiTemplate
     using System.Text.Json.Serialization;
     using ApiTemplate.ViewModels;
 
+    /// <summary>
+    /// Enables faster serialization and de-serialization with fewer allocations by generating source code.
+    /// </summary>
     [JsonSerializable(typeof(Car[]))]
     [JsonSerializable(typeof(Connection<Car>[]))]
     [JsonSerializable(typeof(SaveCar[]))]
