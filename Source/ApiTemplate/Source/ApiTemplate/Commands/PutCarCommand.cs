0eab5bdc9059a08ccca5ac60b6f61c63753672b1
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-08-17 14:46:14 +0100

    Update to latest fluent validation

--- Source/ApiTemplate/Source/ApiTemplate/Commands/PutCarCommand.cs
+++ Source/ApiTemplate/Source/ApiTemplate/Commands/PutCarCommand.cs
@@ -3,26 +3,43 @@ namespace ApiTemplate.Commands;
 using ApiTemplate.Repositories;
 using ApiTemplate.ViewModels;
 using Boxed.Mapping;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 public class PutCarCommand
 {
+    private readonly IActionContextAccessor actionContextAccessor;
     private readonly ICarRepository carRepository;
     private readonly IMapper<Models.Car, Car> carToCarMapper;
     private readonly IMapper<SaveCar, Models.Car> saveCarToCarMapper;
+    private readonly IValidator<SaveCar> saveCarValidator;
 
     public PutCarCommand(
+        IActionContextAccessor actionContextAccessor,
         ICarRepository carRepository,
         IMapper<Models.Car, Car> carToCarMapper,
-        IMapper<SaveCar, Models.Car> saveCarToCarMapper)
+        IMapper<SaveCar, Models.Car> saveCarToCarMapper,
+        IValidator<SaveCar> saveCarValidator)
     {
+        this.actionContextAccessor = actionContextAccessor;
         this.carRepository = carRepository;
         this.carToCarMapper = carToCarMapper;
         this.saveCarToCarMapper = saveCarToCarMapper;
+        this.saveCarValidator = saveCarValidator;
     }
 
     public async Task<IActionResult> ExecuteAsync(int carId, SaveCar saveCar, CancellationToken cancellationToken)
     {
+        var validationResult = this.saveCarValidator.Validate(saveCar);
+        if (!validationResult.IsValid)
+        {
+            var modelState = this.actionContextAccessor.ActionContext!.ModelState;
+            validationResult.AddToModelState(modelState, null);
+            return new BadRequestObjectResult(modelState);
+        }
+
         var car = await this.carRepository.GetAsync(carId, cancellationToken).ConfigureAwait(false);
         if (car is null)
         {
