5e356c729ccb16af6eadffa50886ff24d05ad53d
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2023-06-22 11:24:15 +0100

    Fix JsonSerializerContext

--- Source/OrleansTemplate/Source/OrleansTemplate.Server/AppJsonSerializerContext.cs
+++ Source/OrleansTemplate/Source/OrleansTemplate.Server/AppJsonSerializerContext.cs
@@ -5,7 +5,6 @@ using System.Text.Json.Serialization;
 /// <summary>
 /// Enables faster serialization and de-serialization with fewer allocations by generating source code.
 /// </summary>
-// Todo: Add types to serialize E.g. we have added type object below.
 [JsonSerializable(typeof(object))]
 internal sealed partial class AppJsonSerializerContext : JsonSerializerContext
 {
